b10a0af312be04e31f0c419b78319712eaf4355f
TomaszRutyna <tomek_rutyna@wp.pl>
2026-08-06 22:37:45 +0200

    Time-Deposit-Kata: rest api implementation - additional tests

--- kotlin/src/test/kotlin/org/ikigaidigital/adapter/in/rest/TimeDepositControllerTest.kt
+++ kotlin/src/test/kotlin/org/ikigaidigital/adapter/in/rest/TimeDepositControllerTest.kt
@@ -3,6 +3,7 @@ package org.ikigaidigital.adapter.`in`.rest
 import org.assertj.core.api.Assertions.assertThat
 import org.ikigaidigital.BaseIntegrationTest
 import org.ikigaidigital.adapter.out.persistence.entity.TimeDepositEntity
+import org.ikigaidigital.adapter.rest.model.TimeDepositPage
 import org.ikigaidigital.adapter.rest.model.TimeDepositRequest
 import org.ikigaidigital.adapter.rest.model.TimeDepositResponse
 import org.junit.jupiter.api.Test
@@ -91,5 +92,48 @@ class TimeDepositControllerTest: BaseIntegrationTest() {
 
         assertThat(withdrawal?.amount?.toInt()).isEqualTo(100)
         assertThat(withdrawal?.date).isEqualTo(LocalDate.now())
+    }
+
+    @Test
+    fun shouldFetchTimeDeposits() {
+        //given
+        val storedStudentTimeDeposit = timeDepositRepository.save(
+            TimeDepositEntity(
+                planType = "student",
+                dayOfDeposit = 1,
+                balance = BigDecimal.valueOf(1000),
+                forDate = LocalDate.now().minusDays(10),
+                nextInterestCalculationDate = LocalDate.now().plusDays(20)
+            )
+        )
+
+        val storedPremiumTimeDeposit = timeDepositRepository.save(
+            TimeDepositEntity(
+                planType = "premium",
+                dayOfDeposit = 1,
+                balance = BigDecimal.valueOf(2000),
+                forDate = LocalDate.now().minusDays(10),
+                nextInterestCalculationDate = LocalDate.now().plusDays(20)
+            )
+        )
+        //when
+        val responseString = mockMvc.perform(
+            MockMvcRequestBuilders.get(API_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("size", "10")
+                .queryParam("page", "1")
+        ).andReturn().response.contentAsString
+        //then
+        val response = objectMapper.readValue(responseString, TimeDepositPage::class.java)
+
+        response.content?.first { it.id == storedPremiumTimeDeposit.id }!!.assertWithEntity(storedPremiumTimeDeposit)
+        response.content?.first { it.id == storedStudentTimeDeposit.id }!!.assertWithEntity(storedStudentTimeDeposit)
+    }
+
+    private fun TimeDepositResponse.assertWithEntity(timeDepositEntity: TimeDepositEntity) {
+        assertThat(this.id).isEqualTo(timeDepositEntity.id!!)
+        assertThat(this.planType).isEqualTo(timeDepositEntity.planType)
+        assertThat(this.days).isEqualTo(timeDepositEntity.dayOfDeposit)
+        assertThat(this.amount.toInt()).isEqualTo(timeDepositEntity.balance.toInt())
     }
 }
